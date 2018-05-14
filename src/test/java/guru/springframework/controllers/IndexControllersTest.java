6f8ba1205cd1c25928bc9cd5b1940900051b9b2c
Dave Turner <davetii@gmail.com>
2018-05-13 21:05:11 -0400

    Spring Integration tests

--- src/test/java/guru/springframework/controllers/IndexControllersTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllersTest.java
@@ -10,6 +10,11 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -38,7 +43,8 @@ public class IndexControllersTest {
         Recipe rec1 = new Recipe();
         rec1.setDifficulty(Difficulty.EASY);
         Recipe rec2 = new Recipe();
-        rec1.setDifficulty(Difficulty.MODERATE);
+        rec2.setDifficulty(Difficulty.MODERATE);
+        rec2.setId(new Long("123"));
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(rec1);
         recipes.add(rec2);
@@ -52,8 +58,13 @@ public class IndexControllersTest {
                 captor.capture());
         Set<Recipe> captorResult = captor.getValue();
         assertEquals(2, captorResult.size());
+    }
 
-
-
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"));
     }
 }
---
+++ src/test/java/guru/springframework/repo/UnitOfMeasureRepoTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.repo;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepoTest {
+
+    @Autowired
+    UnitOfMeasureRepo repo;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uom = repo.findByDescription("teaspoon");
+        assertEquals(uom.get().getDescription(), "teaspoon");
+    }
+}
